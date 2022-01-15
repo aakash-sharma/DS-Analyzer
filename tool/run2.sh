79515b6f9e9d9feda4224570b46867eedec9f2da
Aakash Sharma <asharma.com@gmail.com>
2022-01-15 19:45:40 +0000

    added parms

--- tool/run2.sh
+++ tool/run2.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+GPU=$1
+CPU=$2
 
 for arch in 'alexnet' 'resnet18' 'shufflenet_v2_x0_5' 'mobilenet_v2' 'squeezenet1_0'; do
          for batch in 256; do
 	     echo "==============================================="
 	     echo " $batch $arch"
 	     echo "==============================================="
-             python -u harness.py --nproc_per_node=8 -j 28 -b $batch  -a $arch --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+             python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch  -a $arch --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
          done
 done
 
 echo "==============================================="
 echo " 128 squeezenet1_0"
 echo "==============================================="
-python -u harness.py --nproc_per_node=8 -j 28 -b 128  -a squeezenet1_0 --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+python -u harness.py --nproc_per_node=$GPU -j $CPU -b 128  -a squeezenet1_0 --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
 
 echo "==============================================="
 echo " 64 resnet50"
 echo "==============================================="
-python harness.py --nproc_per_node=8 -j 28 -b 64  -a resnet50 --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
+python harness.py --nproc_per_node=$GPU -j $CPU -b 128  -a resnet50 --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
 
 
 echo "==============================================="
 echo " 32 vgg11"
 echo "==============================================="
-python harness.py --nproc_per_node=8 -j 28 -b 32  -a vgg11 --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
+python harness.py --nproc_per_node=$GPU -j $CPU -b 128  -a vgg11 --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
