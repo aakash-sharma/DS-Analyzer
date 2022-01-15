7499bda50db2e6b196bac1ecc0d76453ba208ae8
Aakash Sharma <asharma.com@gmail.com>
2022-01-15 16:07:16 +0000

    initial commit

--- tool/run1.sh
+++ tool/run1.sh
@@ -6,15 +6,15 @@ for arch in 'alexnet' 'resnet18' 'shufflenet_v2_x0_5' 'mobilenet_v2' 'squeezenet
 	     echo "==============================================="
 	     echo " $batch $arch"
 	     echo "==============================================="
-             python -u harness.py --nproc_per_node=1 -j 2 -b $batch  -a $arch --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+             python -u harness.py --nproc_per_node=8 -j 28 -b $batch  -a $arch --prefix results/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
          done
 done
 
 for arch in 'resnet50' 'vgg11'; do
-         for batch in 256; do
+         for batch in 128; do
 	     echo "==============================================="
 	     echo " $batch $arch"
 	     echo "==============================================="
-             python harness.py --nproc_per_node=1 -j 2 -b $batch  -a $arch --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
+             python harness.py --nproc_per_node=8 -j 28 -b $batch  -a $arch --prefix results/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
          done
 done
