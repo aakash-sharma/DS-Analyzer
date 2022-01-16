3b0eb1752680932bce3e17ba399cd40c5b5ed692
Aakash Sharma <asharma.com@gmail.com>
2022-01-16 09:07:29 -0500

    added comparison code

--- tool/analyze.py
+++ tool/analyze.py
@@ -13,6 +13,7 @@ gpu_map = {
         "p2.2xlarge" : "gpus-1",
         "p2.8xlarge" : "gpus-8"}
 
+instances = []
 
 def process_json(model, gpu, json_path):
 
@@ -71,13 +72,62 @@ def plotModels(instance):
     axs1[1].set_xticks(X_axis)
     axs1[1].set_xticklabels(X)
     axs1[1].set_xlabel("Models")
-    axs1[1].set_ylabel("Time")
+    axs1[1].set_ylabel("Percentage")
     axs1[1].legend()
 
     fig1.suptitle("Stall analysis " + instance)
-    #plt.xticks(X_axis, X)
     plt.show()
 
+def compare():
+
+    models = list(stats.keys())
+
+    for instance in instances:
+
+        gpu = gpu_map[instance]
+
+        for model in models:
+
+            if gpu not in stats[model]:
+                del stats[model]
+
+
+    fig1, axs1 = plt.subplots(2, 1)
+
+    X = [model for model in stats.keys()]
+    X_axis = np.arange(len(X))
+
+    diff = 0
+
+    for instance in instances:
+        
+        gpu = gpu_map[instance]
+        
+        Y_PREP_STALL_PCT = [stats[model][gpu]["PREP_STALL_PCT"] for model in X]
+        Y_FETCH_STALL_PCT = [stats[model][gpu]["FETCH_STALL_PCT"] for model in X]
+
+        axs1[0].bar(X_axis-0.2 + diff , Y_PREP_STALL_PCT, 0.2, label = instance)
+        axs1[1].bar(X_axis-0.2 + diff, Y_FETCH_STALL_PCT, 0.2, label = instance)
+
+        diff += 0.2
+
+    axs1[0].set_xticks(X_axis)
+    axs1[0].set_xticklabels(X)
+    axs1[0].set_xlabel("Models")
+    axs1[0].set_ylabel("Percentage")
+    axs1[0].set_title("Prep stall comparison")
+    axs1[0].legend()
+
+
+    axs1[1].set_xticks(X_axis)
+    axs1[1].set_xticklabels(X)
+    axs1[1].set_xlabel("Models")
+    axs1[1].set_ylabel("Percentage")
+    axs1[1].set_title("Fetch stall comparison")
+    axs1[1].legend()
+
+    fig1.suptitle("Stall comparison" )
+    plt.show()
 
 
 def main():
@@ -85,26 +135,29 @@ def main():
     if len(sys.argv) <= 1:
         return
 
-    instance = sys.argv[1]
-    result_path = "results-" + instance + "/" + "dali-gpu"
+    for instance in sys.argv[1:]:
+        instances.append(instance)
+        result_path = "results-" + instance + "/" + "dali-gpu"
 
-    model_paths = [os.path.join(result_path, o) for o in os.listdir(result_path) if os.path.isdir(os.path.join(result_path,o))]
+        model_paths = [os.path.join(result_path, o) for o in os.listdir(result_path) if os.path.isdir(os.path.join(result_path,o))]
 
-    for model_path in model_paths:
-        model = model_path.split('/')[-1]
-        model_path_ = model_path + "/jobs-1"
-        gpu_paths = [os.path.join(model_path_, o) for o in os.listdir(model_path_) if os.path.isdir(os.path.join(model_path_,o))]
-        for gpu_path in gpu_paths:
-            gpu = gpu_path.split('/')[-1]
-            cpu_paths = [os.path.join(gpu_path, o) for o in os.listdir(gpu_path) if os.path.isdir(os.path.join(gpu_path,o))]
-            for cpu_path in cpu_paths:
-                json_path = cpu_path + "/MODEL.json"
-                if not os.path.isfile(json_path):
-                    continue
+        for model_path in model_paths:
+            model = model_path.split('/')[-1]
+            model_path_ = model_path + "/jobs-1"
+            gpu_paths = [os.path.join(model_path_, o) for o in os.listdir(model_path_) if os.path.isdir(os.path.join(model_path_,o))]
+            for gpu_path in gpu_paths:
+                gpu = gpu_path.split('/')[-1]
+                cpu_paths = [os.path.join(gpu_path, o) for o in os.listdir(gpu_path) if os.path.isdir(os.path.join(gpu_path,o))]
+                for cpu_path in cpu_paths:
+                    json_path = cpu_path + "/MODEL.json"
+                    if not os.path.isfile(json_path):
+                        continue
 
-                process_json(model, gpu, json_path)
+                    process_json(model, gpu, json_path)
 
-    plotModels(instance)
+        #plotModels(instance)
+
+    compare()
 
 
 if __name__ == "__main__":
