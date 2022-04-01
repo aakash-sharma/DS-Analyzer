357b73390fbad564dfc9376d9af5166adc4b27c5
Aakash Sharma <asharma.com@gmail.com>
2022-04-01 03:56:09 +0000

    bug fix

--- tool/run2_dist.sh
+++ tool/run2_dist.sh
@@ -40,7 +40,7 @@ echo "==============================================="
 batch=64
 python harness.py --nproc_per_node=$GPU -j $CPU -b 32  -a vgg11 --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches --num_minibatches $((SAMPLES / batch / GPU / 2)) --prefix ${PREFIX}/${INSTANCE}/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --dali_cpu --amp --noeval  --data /home/ubuntu/ImageNet_Datasets  >> ds_log 2>&1
 
-if $NODE_RANK == 0; then
+if [[ $NODE_RANK == 0 ]]; then
 	exit
 fi
 
