7ed3b381e00118d326db5b0f8cdec895a7f1e6de
Aakash Sharma <asharma.com@gmail.com>
2022-04-04 19:10:48 -0400

    cont

--- scripts/runCmdOnSlaves.sh
+++ scripts/runCmdOnSlaves.sh
@@ -3,10 +3,11 @@
 range=$1
 i=0
 
-declare -A slaves=( ["slave0"] = "172.31.65.5")
+declare -A slaves=( ["slave0"]="172.31.65.5" )
 
 while [ $i -lt $range ];
 do
-    ssh -o "StrictHostKeyChecking no" ${slave}$i "/proj/scheduler-PG0/scripts/limitCPU.sh > /users/aakashsh/limit.log 2>&1 &"
+    #ssh -o "StrictHostKeyChecking no" ${slave}$i "/proj/scheduler-PG0/scripts/limitCPU.sh > /users/aakashsh/limit.log 2>&1 &"
+	echo "${slaves[0]}"
     i=$((i+1))
 done
