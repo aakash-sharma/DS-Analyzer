fa1ff62332a7b3591bc7927e2beb473bdcbe2099
Aakash Sharma <asharma.com@gmail.com>
2022-04-09 01:08:25 -0400

    new configs batch

--- tool/run_rerun.sh
+++ tool/run_rerun.sh
@@ -6,17 +6,19 @@ NNODES=$3
 RESULT_DIR=$4
 INSTANCE=$5
 
-for arch in 'alexnet' 'resnet18' 'shufflenet_v2_x0_5' 'mobilenet_v2' 'squeezenet1_0'; do
+for arch in 'alexnet' 'resnet18'; do
 	     echo "==============================================="
 	     echo "$arch"
 	     echo "==============================================="
-		 python3 -u harness.py --nproc_per_node=$GPU -j $CPU -a $arch --nnodes=$NNODES --steps RUN1 RUN2 RUN3 --resume_dir ${RESULT_DIR}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+		 python3 -u harness.py --nproc_per_node=$GPU -j $CPU -a $arch --nnodes=$NNODES -b 128 --steps RUN1 RUN2 RUN3 --resume_dir ${RESULT_DIR}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
 done
 
-for arch in 'resnet50' 'vgg11'; do
+python3 -u harness.py --nproc_per_node=$GPU -j $CPU -a shufflenet_v2_x0_5 --nnodes=$NNODES -b 1024 --steps RUN1 RUN2 RUN3 --resume_dir ${RESULT_DIR}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+
+for arch in 'mobilenet_v2' 'squeezenet1_0' 'resnet50' 'vgg11'; do
 	     echo "==============================================="
 	     echo "$arch"
 	     echo "==============================================="
-		 python3 -u harness.py --nproc_per_node=$GPU -j $CPU -a $arch --nnodes=$NNODES --steps RUN1 RUN2 RUN3 --resume_dir ${RESULT_DIR}/${INSTANCE}/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py 
+		 python3 -u harness.py --nproc_per_node=$GPU -j $CPU -a $arch --nnodes=$NNODES -b 64 --steps RUN1 RUN2 RUN3 --resume_dir ${RESULT_DIR}/${INSTANCE}/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py 
 done
 
