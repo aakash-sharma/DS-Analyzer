dc7822f267fbc823b59f5a29d6f94955f1f5bbe4
Aakash Sharma <asharma.com@gmail.com>
2022-04-15 11:06:22 -0400

    bug fix

--- scripts/run_p3.sh
+++ scripts/run_p3.sh
@@ -14,7 +14,7 @@ for arch in 'alexnet' 'resnet18' 'shufflenet_v2_x0_5' 'mobilenet_v2' 'squeezenet
 				 echo "==============================================="
 				 echo " $batch $arch"
 				 echo "==============================================="
-				 python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch  -a $arch --num_minibatches $((SAMPLES / batch / GPU )) --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds.log 2>&1
+				 python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch  -a $arch --num_minibatches $((SAMPLES / batch / GPU )) --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
          done
 done
 
@@ -23,7 +23,7 @@ for arch in 'resnet50' 'vgg11'; do
 				 echo "==============================================="
 				 echo " $batch $arch"
 				 echo "==============================================="
-				 python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch  -a $arch --num_minibatches $((SAMPLES / batch / GPU )) --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds.log 2>&1
+				 python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch  -a $arch --num_minibatches $((SAMPLES / batch / GPU )) --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
          done
 done
-cp ds.log ${PREFIX}/${INSTANCE}
+cp ds_log ${PREFIX}/${INSTANCE}
