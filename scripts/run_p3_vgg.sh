54106d33ba0dc8c7bc4c0e742fdfa533aec27a93
Aakash Sharma <asharma.com@gmail.com>
2022-05-09 00:18:06 -0400

    improvements

--- scripts/run_p3_vgg.sh
+++ scripts/run_p3_vgg.sh
@@ -12,14 +12,46 @@ SAMPLES=1281166
 
 cd ~/DS-Analyzer/tool
 
-for arch in 'vgg13' 'vgg16' 'vgg19' 'resnet34' 'resnet101' 'resnet152'; do
+for arch in 'resnet18'; do
 	for batch in 80; do
 		d=`date`
 		echo "==============================================="
 	    echo " $batch $arch $d"
 	    echo "==============================================="
 
-		if ((NNODES < 1)); then 
+		if ((NNODES < 2)); then 
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+		else
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+		fi
+	done
+done
+
+
+for arch in 'resnet18' 'resnet50' 'vgg11'; do
+	for batch in 48; do
+		d=`date`
+		echo "==============================================="
+	    echo " $batch $arch $d"
+	    echo "==============================================="
+
+		if ((NNODES < 2)); then 
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+		else
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+		fi
+	done
+done
+
+
+for arch in 'vgg13' 'vgg16' 'vgg19' 'resnet34' 'resnet101' 'resnet152'; do
+	for batch in 32 48 64; do
+		d=`date`
+		echo "==============================================="
+	    echo " $batch $arch $d"
+	    echo "==============================================="
+
+		if ((NNODES < 2)); then 
 			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
 		else
 			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / GPU / 2)) --steps RUN0 RUN1 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
