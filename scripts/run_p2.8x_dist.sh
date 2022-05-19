13ef4c9df6fc1afa380d42a19a6ac684e3f44f96
Aakash Sharma <asharma.com@gmail.com>
2022-05-19 18:41:19 -0400

    bug fix

--- scripts/run_p2.8x_dist.sh
+++ scripts/run_p2.8x_dist.sh
@@ -17,7 +17,7 @@ for arch in 'mobilenet_v2' 'resnet18' 'alexnet' 'shufflenet_v2_x0_5' 'squeezenet
 	echo "==============================================="
 	echo "128 $arch $d"
 	echo "==============================================="
-	python -u harness.py --nproc_per_node=4 -j 16 -b 256 -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / 256 / NUM_GPUS)) --synthetic_div_factor $DIV_FACTOR --steps RUN0 RUN1 --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+	python -u harness.py --nproc_per_node=4 -j 16 -b 128 -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / 128 / NUM_GPUS)) --synthetic_div_factor $DIV_FACTOR --steps RUN1 --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
 done
 
 
