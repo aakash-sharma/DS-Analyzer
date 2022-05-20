86818e6b3de4f5e96f93df9407fdc53659c57a6b
Aakash Sharma <asharma.com@gmail.com>
2022-05-20 03:48:32 -0400

    bug fix

--- scripts/run_p2_dist.sh
+++ scripts/run_p2_dist.sh
@@ -15,14 +15,25 @@ NUM_GPUS=$((8 * NNODES))
 cd ~/DS-Analyzer/tool
 
 for arch in 'mobilenet_v2' 'resnet18' 'alexnet' 'shufflenet_v2_x0_5' 'squeezenet1_0'; do
-	d=`date`
-	for batch in 32 64 128 256; do
+	for batch in 32 64 96 128; do
+		d=`date`
 		echo "==============================================="
-		echo "128 $batch $arch $d"
+		echo "$batch $arch $d"
 		echo "==============================================="
-			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / NUM_GPUS)) --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / NUM_GPUS)) --steps RUN1 RUN2 RUN3 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
 	done
 done
+
+for arch in 'mobilenet_v2' 'resnet18' 'alexnet' 'shufflenet_v2_x0_5' 'squeezenet1_0'; do
+	for batch in 32 64 96 128; do
+		d=`date`
+		echo "==============================================="
+		echo "$batch $arch $d"
+		echo "==============================================="
+			python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / NUM_GPUS)) --steps RUN0 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+	done
+done
+
 
 
 cd ${PREFIX}
