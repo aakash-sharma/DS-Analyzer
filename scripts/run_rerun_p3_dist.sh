b26bda4369ecbdbfb0b634eb7f739f2a6d4a6994
Aakash Sharma <asharma.com@gmail.com>
2022-05-26 20:38:19 -0400

    adding rerun scripts

--- scripts/run_rerun_p3_dist.sh
+++ scripts/run_rerun_p3_dist.sh
@@ -2,17 +2,47 @@
 
 cd ~/DS-Analyzer/tool
 
-NNODES=2
+<<'EOF'
+for arch in 'alexnet' 'resnet18' 'shufflenet_v2_x0_5' 'mobilenet_v2' 'squeezenet1_0'; do
+		for batch in 32 64 80 128 256; do
+	    	echo "==============================================="
+		    echo "$batch $arch"
+		    echo "==============================================="
+			python3 -u harness.py --nproc_per_node=4 -j 16 -a $arch --nnodes=2 -b $batch --steps RUN0 RUN1 RUN2 RUN3 --resume_dir results/results-p3-dist/p3.8xlarge_2/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+		done
+done
 
-python3 -u harness.py --nproc_per_node=4 -j 16 -a alexnet --nnodes=$NNODES -b 256 --resume_dir results/results-p3/p3.8xlarge_2/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+for arch in 'vgg11' 'resnet50'; do
+		for batch in 80; do
+	    	echo "==============================================="
+		    echo "$batch $arch"
+		    echo "==============================================="
+			python3 -u harness.py --nproc_per_node=4 -j 16 -a $arch --nnodes=2 -b $batch --steps RUN0 RUN1 RUN2 RUN3 --resume_dir results/results-p3-dist/p3.8xlarge_2/dali-cpu/  image_classification/pytorch-imagenet-dali-mp.py 
+		done
+done
 
-python3 -u harness.py --nproc_per_node=4 -j 16 -a resnet18 --nnodes=$NNODES -b 128 --resume_dir results/results-p3/p3.8xlarge_2/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
-
-python3 -u harness.py --nproc_per_node=4 -j 16 -a shufflenet_v2_x0_5 --nnodes=$NNODES -b 256 --resume_dir results/results-p3/p3.8xlarge_2/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
-
-python3 -u harness.py --nproc_per_node=4 -j 16 -a squeezenet1_0 --nnodes=$NNODES -b 256 --resume_dir results/results-p3/p3.8xlarge_2/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
 
 
+for arch in 'vgg11' 'vgg13' 'vgg16' 'vgg19' 'resnet18' 'resnet50' 'resnet34' 'resnet18' 'resnet101' 'resnet152'; do
+		for batch in 32 48 64 80; do
+	    echo "==============================================="
+	    echo "$batch $arch"
+	    echo "==============================================="
+			python3 -u harness.py --nproc_per_node=4 -j 16 -a $arch --nnodes=1 -b $batch --steps RUN0 RUN1 --resume_dir results/results-p3/p3.8xlarge/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+			python3 -u harness.py --nproc_per_node=8 -j 32 -a $arch --nnodes=1 -b $batch --steps RUN0 RUN1 --resume_dir results/results-p3/p3.16xlarge/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
 
-#for arch in 'resnet50' 'vgg11'; do
+		done
+done
 
+EOF
+
+for arch in 'resnet18' 'resnet50' 'resnet34' 'resnet18' 'resnet101' 'resnet152'; do
+		for batch in 32 80; do
+	    echo "==============================================="
+	    echo "$batch $arch"
+	    echo "==============================================="
+			python3 -u harness.py --nproc_per_node=4 -j 16 -a $arch --nnodes=1 -b $batch --steps RUN0 RUN1 --resume_dir results/results-p3-synthetic/p3.8xlarge_noResidue/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+#			python3 -u harness.py --nproc_per_node=8 -j 32 -a $arch --nnodes=1 -b $batch --steps RUN0 RUN1 --resume_dir results/results-p3/p3.16xlarge/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py 
+
+		done
+done
