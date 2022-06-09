1e3615fbb8424512b6e3e2134a77da3d45cffd1e
Aakash Sharma <asharma.com@gmail.com>
2022-06-08 21:47:01 -0400

    enhancements

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-root=~/DS-Analyzer/tool/results/results-p3-synthetic/p3.16xlarge_noResidue/
+#root=~/DS-Analyzer/tool/results/results-p3-synthetic/p3.16xlarge_noResidue/
+root=~/DS-Analyzer/tool/results/results-p3-synthetic/results-p3-synthetic-resnet-noBn
 cd $root
 
 <<'EOF'
 
 EOF
 
-for repeat in 'repeat-0' 'repeat-1' 'repeat-2'; do
+for instance in 'p3.16xlarge' 'p3.8xlarge_2'; do
 
-		cd $repeat/dali-gpu
+		for repeat in 'repeat-0' 'repeat-1' 'repeat-2'; do
 
-		rm -rf noBN_*
-		rm -rf vgg*
-		rm -rf resnet*
+				cd $instance/$repeat/dali-gpu
+
+				#rm -rf noBN_*
+				rm -rf noRes_*
+		
+				rm -rf vgg*
+				rm -rf resnet*
 
 
-		#for file in noBN_*; do
-		for file in noResidue_*; do
+				for file in noBN_*; do
+				#for file in noResidue_*; do
 
-				new_file=${file//noResidue_}
-				echo $file $new_file
-				mv "$file" $new_file
+						#new_file=${file//noResidue_}
+						new_file=${file//noBN_}
+						echo $file $new_file
+						mv "$file" $new_file
+				done
+
+				cd $root
 		done
-
-		cd $root
 done
