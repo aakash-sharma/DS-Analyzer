972a69167183d80f01235263c3fcb79324dc0e23
Aakash Sharma <asharma.com@gmail.com>
2022-10-01 03:09:16 +0000

    updated

--- tool/run_bert.sh
+++ tool/run_bert.sh
@@ -1,1 +1,1 @@
-python harness.py --nproc_per_node=8 -j 8 -a BERT -b 4 --synthetic_div_factor 1 --num_minibatches 678 --steps  RUN0 RUN1 --prefix results/BERT/ BERT/bert_squad_dist_stash.py
+python harness.py --nproc_per_node=8 -j 8 -a BERT -b 4 --synthetic_div_factor 1 --num_minibatches 678 --steps  RUN0 RUN1 --prefix results/BERT/p3.24xlarge BERT/bert_squad_dist_stash.py
