8c5b4c851f9811ede0913e6a9f95b83813d27bbb
Aakash Sharma <asharma.com@gmail.com>
2022-10-15 16:01:13 +0000

    bug fix

--- scripts/run_p3.16x_dist.sh
+++ scripts/run_p3.16x_dist.sh
@@ -16,15 +16,15 @@ echo $NUM_GPUS
 cd ~/DS-Analyzer/tool
 
 
-#for arch in 'resnet50' 'vgg11'; do
-#	for batch in 80; do
-#	    d=`date`
-#	    echo "==============================================="
-#	    echo " $batch $arch $d"
-#	    echo "==============================================="
-#		python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / NUM_GPUS)) --steps RUN1 RUN2 RUN3 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
-#	done
-#done
+for arch in 'resnet50' 'vgg11'; do
+	for batch in 80; do
+	    d=`date`
+	    echo "==============================================="
+	    echo " $batch $arch $d"
+	    echo "==============================================="
+		python -u harness.py --nproc_per_node=$GPU -j $CPU -b $batch -a $arch --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((SAMPLES / batch / NUM_GPUS)) --steps RUN1 RUN2 RUN3 --synthetic_div_factor $DIV_FACTOR --prefix ${PREFIX}/${INSTANCE}/dali-gpu/  image_classification/pytorch-imagenet-dali-mp.py --amp --noeval  --data /home/ubuntu/ImageNet_Datasets >> ds_log 2>&1
+	done
+done
 
 python harness.py --nproc_per_node=$GPU -j $CPU -a BERT -b 4 --nnodes=$NNODES --master_addr=$MASTER --master_port=11111 --node_rank=$NODE_RANK --num_minibatches $((5424 / NUM_GPUS)) --synthetic_div_factor 1 --steps  RUN2 RUN3 --prefix ${PREFIX}/${INSTANCE} BERT/bert_squad_dist_stash.py >> ds_log 2>&1
 
